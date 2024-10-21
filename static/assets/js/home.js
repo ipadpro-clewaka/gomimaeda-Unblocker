1dd17f965c084a7d7d2982cb1649779a85a76fb2
Minotaur-ZAOU <ichiharuhidaka@gmail.com>
2024-10-21 20:31:48 +0900

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -37,15 +37,15 @@ setInterval(updateClock, 1000);
 
 
 const options = [
-    "Right-Click to access more features",
-    "Set a custom background in settings.",
-    "Tab Cloaking is highly recommended",
-    "About:Blank Cloak is highly recommended",
-    "Finally, an actual site unblocker that works fast!",
-    "It is not recommended to share this link with anyone.",
-    "discord.gg/unblocking for more",
-    "© Copyright 2024 Doge Network. All Rights Reserved.",
-    "Customize Doge by going to Settings > Themes",
+    "Do you know Ninja?",
+    "This network offers ninja-like stealth and speed.",
+    "Thank you DogeUnblocker.",
+    "Made by proxy_minotaur",
+    "Finally, an actual Ninja_Network that works fast!",
+    "On my way.",
+    "say happy",
+    "© Copyright 2024 Ninja Network. All Rights Reserved.",
+    "Customize Ninja by going to Settings > Themes",
 ];
 
 function getRandomOption() {
