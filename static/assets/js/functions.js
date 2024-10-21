9f959fb2e90b427a17a134af2dfb5de0706c10d9
Minotaur-ZAOU <ichiharuhidaka@gmail.com>
2024-10-21 20:17:38 +0900

    Update functions.js

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -117,7 +117,7 @@ function tabCloak() {
 }
 
 function disableTabCloak() {
-  var newTitle = 'Doge | V4';
+  var newTitle = 'Net_Ninja';
   var newIcon = '/assets/img/doge.jpg';
 
   localStorage.setItem('tabTitle', newTitle);
